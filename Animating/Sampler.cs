a82e228c5fb626432594e50a0b969321e8fb0c36
Logan <qingwabote@126.com>
2026-09-22 16:10:14 +0800

    use Bastard.Sampler

--- Animating/Sampler.cs
+++ Animating/Sampler.cs
@@ -8,93 +8,19 @@ namespace Graphix
         public BlobArray<float> Times;
         public BlobArray<float> Values;
 
-        public readonly static float EPSILON = 1e-6f;
-
-        public int Seek(float value)
+        public unsafe float Float(float time)
         {
-            if (value < Times[0])
-            {
-                return 0;
-            }
-
-            if (value > Times[Times.Length - 1])
-            {
-                return Times.Length - 1;
-            }
-
-            int head = 0;
-            int tail = Times.Length - 1;
-            while (head <= tail)
-            {
-                int mid = (head + tail) >> 1;
-                float res = Times[mid];
-                if ((value + EPSILON) < res)
-                {
-                    tail = mid - 1;
-                }
-                else if ((value - EPSILON) > res)
-                {
-                    head = mid + 1;
-                }
-                else
-                {
-                    return mid;
-                }
-            }
-            return ~head;
-        }
-
-        public float Float(float time)
-        {
-            int index = Seek(time);
-            if (index >= 0)
-            {
-                return Values[index];
-            }
-
-            int next = ~index;
-            int prev = next - 1;
-
-            float t = (time - Times[prev]) / (Times[next] - Times[prev]);
-            return math.lerp(Values[prev], Values[next], t);
+            return Bastard.Sampler.Float((float*)Times.GetUnsafePtr(), (float*)Values.GetUnsafePtr(), Times.Length, time);
         }
 
         public unsafe float3 Vec3(float time)
         {
-            float3* output = (float3*)Values.GetUnsafePtr();
-
-            int index = Seek(time);
-            if (index >= 0)
-            {
-                return *(output + index);
-            }
-            else
-            {
-                int next = ~index;
-                int prev = next - 1;
-
-                float t = (time - Times[prev]) / (Times[next] - Times[prev]);
-                return math.lerp(*(output + prev), *(output + next), t);
-            }
+            return Bastard.Sampler.Vec3((float*)Times.GetUnsafePtr(), (float3*)Values.GetUnsafePtr(), Times.Length, time);
         }
 
         public unsafe quaternion Quat(float time)
         {
-            quaternion* output = (quaternion*)Values.GetUnsafePtr();
-
-            int index = Seek(time);
-            if (index >= 0)
-            {
-                return *(output + index);
-            }
-            else
-            {
-                int next = ~index;
-                int prev = next - 1;
-
-                float t = (time - Times[prev]) / (Times[next] - Times[prev]);
-                return math.slerp(*(output + prev), *(output + next), t);
-            }
+            return Bastard.Sampler.Quat((float*)Times.GetUnsafePtr(), (quaternion*)Values.GetUnsafePtr(), Times.Length, time);
         }
     }
 }
